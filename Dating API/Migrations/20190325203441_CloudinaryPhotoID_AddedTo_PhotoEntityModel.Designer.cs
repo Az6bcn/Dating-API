9c738876019b6f2eba2e565d733e99729f0fd44a
Azeez Odu <az6bcn@gmail.com>
2019-03-25 20:42:05 +0000

    Cloudinary Package and Migration

--- Dating API/Migrations/20190325203441_CloudinaryPhotoID_AddedTo_PhotoEntityModel.Designer.cs
+++ Dating API/Migrations/20190325203441_CloudinaryPhotoID_AddedTo_PhotoEntityModel.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace DatingAPI.Migrations
 {
     [DbContext(typeof(DataDbContext))]
-    partial class DataDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190325203441_CloudinaryPhotoID_AddedTo_PhotoEntityModel")]
+    partial class CloudinaryPhotoID_AddedTo_PhotoEntityModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Dating API/Migrations/20190325203441_CloudinaryPhotoID_AddedTo_PhotoEntityModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace DatingAPI.Migrations
+{
+    public partial class CloudinaryPhotoID_AddedTo_PhotoEntityModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CloudinaryID",
+                table: "Photos",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CloudinaryID",
+                table: "Photos");
+        }
+    }
+}
