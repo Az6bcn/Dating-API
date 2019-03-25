75224dc6f02a61353b205f4ec71e03affba3b0fa
Azeez Odu <az6bcn@gmail.com>
2019-03-25 20:29:55 +0000

    Cloudinary Helper

--- Dating API/Helpers/CloudinaryHelper.cs
+++ Dating API/Helpers/CloudinaryHelper.cs
@@ -1,6 +1,10 @@
-﻿using DatingAPI.Model;
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using DatingAPI.Model;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +13,28 @@ namespace DatingAPI.Helpers
     public class CloudinaryHelper
     {
         private CloudinarySettings _cloudinarySettings;
-        public CloudinaryHelper(CloudinarySettings cloudinarySettings)
+     
+        public CloudinaryHelper(IOptions<CloudinarySettings> cloudinarySettings)
         {
-            _cloudinarySettings = cloudinarySettings;
+            _cloudinarySettings = cloudinarySettings.Value;
+        }
+
+        public UploadResult UploadPhotoToCloudinary(string photoName, Stream photoStream)
+        {
+            Account cloudinarAaccount = new Account(_cloudinarySettings.CloudName, _cloudinarySettings.ApiKey,
+                _cloudinarySettings.ApiSecret );
+
+            Cloudinary cloudinary = new Cloudinary(cloudinarAaccount);
+
+
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(photoName, photoStream)
+            };
+
+            var uploadResult = cloudinary.Upload(uploadParams);
+
+            return uploadResult;
         }
     }
 }
